f8e8b896c5bd3193c3c360d94a25625da6829e2b
Gabriel Moraga <agarom97@gmail.com>
2021-03-18 18:34:02 +0100

    Esercizio + bonus

---
+++ database/migrations/2021_03_18_151316_create_beers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('quantity', 255);
+            $table->float('volume', 4, 1);
+            $table->string('image', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
---
+++ resources/views/beers/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+  <table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($beers as $beer)
+        <tr>
+          <th scope="row">{{$beer->id}}</th>
+          <td>{{$beer->name}}</td>
+          <td>{{$beer->quantity}}</td>
+          <td>{{$beer->volume}}</td>
+          <td><img src="{{asset($beer->image)}}" alt="{{$beer->name}}" height="150"></td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+</body>
+</html>
